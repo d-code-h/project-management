6f1213a34a8dd92519f50fd94c2bb5a914d397b1
Yunus Habeeb <habeebdh1@gmail.com>
2025-01-10 03:48:15 +0100

    Add server setup with Express, Prisma, and initial routes for projects and tasks

---
+++ server/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import projectRoutes from './routes/projectRoutes';
+import taskRoutes from './routes/taskRoutes';
+// import morgan from 'morgan';
+
+// Configurations
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(helmet());
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: 'cross-origin',
+  }),
+);
+// // app.use(morgan('common'));
+app.use(cors());
+
+// Route imports
+app.use('/projects', projectRoutes);
+app.use('/tasks', taskRoutes);
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('This is home route');
+});
+
+// Server
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
