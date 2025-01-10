8fa41a22f8a20a5b3f46ec51c8bf700a70318715
Yunus Habeeb <habeebdh1@gmail.com>
2025-01-10 17:55:44 +0100

    Add Teams management with TeamsWrapper component and API integration

---
+++ client/app/teams/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TeamsWrapper from "../components/TeamsWrapper";
+
+const Teams = () => {
+  return <TeamsWrapper />;
+};
+
+export default Teams;
--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import projectRoutes from './routes/projectRoutes';
 import taskRoutes from './routes/taskRoutes';
 import searchRoutes from './routes/searchRoutes';
 import userRoutes from './routes/userRoutes';
+import teamRoutes from './routes/teamRoutes';
 // import morgan from 'morgan';
 
 // Configurations
@@ -26,6 +27,7 @@ app.use('/projects', projectRoutes);
 app.use('/tasks', taskRoutes);
 app.use('/search', searchRoutes);
 app.use('/users', userRoutes);
+app.use('/teams', teamRoutes);
 
 // Routes
 app.get('/', (req, res) => {
